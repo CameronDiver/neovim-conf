f80ad4e3ddebc84918567e8675a829d61e358b08
Cameron Diver <cameron@pipeline.gg>
2022-04-25 14:09:52 +0100

    Colour scheme changes and diagnostics revert

--- lua/core/colours.lua
+++ lua/core/colours.lua
@@ -1,5 +1,9 @@
-require('mini.base16').setup({
-  palette = require('mini.base16').mini_palette('#112641', '#e2e98f', 75), name='minischeme', use_cterm=true
-})
+-- require('mini.base16').setup({
+--   palette = require('mini.base16').mini_palette('#112641', '#e2e98f', 75), name='minischeme', use_cterm=true
+-- })
 
-vim.cmd("colorscheme onedark")
+vim.termguicolors = true
+vim.cmd("colorscheme material")
+vim.g.material_style = "deep ocean"
+
+
